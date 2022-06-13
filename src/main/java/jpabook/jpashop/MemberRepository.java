b1b74390c7864a4c5150e2b734ee1591bce6c523
awbwd4 <awbwd4@gmail.com>
2022-06-13 09:51:14 +0900

    22.06.11 h2 config

---
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop;
+
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext // 엔티티 매니저가 주입됨.
+    private EntityManager em;
+
+    public Long save(Member member) {
+        em.persist(member); // 멤버 객체 저
+        return member.getId();
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+        //아마 Member.classs 타입의 저장된 객체를 id를 통해 찾으라는것 같음.
+    }
+
+
+}
---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,52 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)//junit에게 스프링 관련된 테스트를 한다고 알려줌
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(value = false)
+    public void testMember() throws Exception{
+        //given
+
+        //        ApplicationContext ac = new AnnotationConfigApplicationContext(Member.class);
+        //        Member member = ac.getBean(Member.class);
+        //        member.setUsername("memberA");
+                //멤버는 스프링 빈으로 관리되지 않음. 싱글톤x
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long savedId = memberRepository.save(member);
+        Member foundMember = memberRepository.find(savedId);
+
+        //then
+
+        Assertions.assertThat(foundMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(foundMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(foundMember).isEqualTo(member);
+
+        System.out.println("foundMember == member" + (foundMember==member));
+
+
+    }
+
+
+}
